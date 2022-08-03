b2ed5a195b9ec9ec9ff59afd5b8f458f7affa826
Dan Scott <dscott304@gmail.com>
2022-08-03 19:49:38 +0100

    cba linting

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  settings: {
-      react: {
-          version: require('./package.json').peerDependencies.react,
-      },
-  }
-};
